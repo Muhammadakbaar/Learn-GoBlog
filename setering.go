9dfa6eade24847b59c1de77617801a2a1ec43abb
muhammadakbaar <mohammadakbaar11@gmail.com>
2020-09-25 13:10:28 +0700

    data type string

---
+++ setering.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	fmt.Println("Muhammad")
+	fmt.Println("Akbar")
+	fmt.Println("Hakim")
+
+	fmt.Println(len("Muhammad"))
+	fmt.Println("Akbar" [0])
+	fmt.Println("Hakim"[1])
+}
