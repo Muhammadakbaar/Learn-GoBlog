ea49216258366a848202842c95781349a77229df
muhammadakbaar <mohammadakbaar11@gmail.com>
2020-10-13 16:46:26 +0700

    update learn go blokc

--- slice.go
+++ slice.go
@@ -1,7 +1,42 @@
 package main
 
+import "fmt"
+
 func main()  {
+	var month = [...]string{
+		"Januari",
+		"Februari",
+		"Maret",
+		"April",
+		"Mei",
+		"Juni",
+		"Juli",
+		"Agustus",
+		"September",
+		"Oktober",
+		"November",
+		"Desember",
+	}
+	var slice1 = month[4:7]
+	fmt.Println(slice1)
+	fmt.Println(len(slice1))
+	fmt.Println(cap(slice1))
+
+	//month[5] = "Diubah"
+	//fmt.Println(slice1)
+
+	slice1[0] = "Mei Ubah"
+	fmt.Println(month)
 
 
+	var slice2 = month[2:4]
+	fmt.Println(slice2) 
 
+	var slice3 = append(slice2, "Akbar")
+	fmt.Println(slice3)
+	slice3[1] = "Bukan Desember"
+	fmt.Println(slice3)
+
+	fmt.Println(slice2)
+	fmt.Println(month)
 }
