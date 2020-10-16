c467173fe2f822c63e8b7d134e17c1c7f98de0bf
muhammadakbaar <mohammadakbaar11@gmail.com>
2020-10-16 16:17:09 +0700

    update for loops

--- for.go
+++ for.go
@@ -1,5 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
-	
-}
+	counter1 := 1
+
+	for counter1 <= 5{
+		fmt.Println("Perulangan Counter 1 = ", counter1)
+		counter1++
+	}
+	fmt.Println("")
+
+	for counter2 := 1; counter2 <= 5; counter2++{
+		fmt.Println("Perulangan Counter 2 = ", counter2)
+	}
+	slice := [] string{"Muhammad", "Akbar", "Hakim"}
+
+	fmt.Println(slice)
+} 
